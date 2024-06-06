0db7ee223d54765bfa331bbeded239532267b6cb
PEREZ GARCIA ULISES DAVID <168507255+ULISES-DAVID@users.noreply.github.com>
2024-06-06 11:43:24 -0600

    Update act14.cpp

---
+++ act14.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n1,n2;
+
+cout<<"dime tú primer numero";
+cin>>n1;
+cout<<"dime tú segundo numero";
+cin>>n2;
+
+if(n1>n2){;
+cout<<"el mayor es"<<n1<<endl;
+}
+ else if(n1<n2){;
+cout<<"el menor es"<<n2<<endl;
+}
+else {;
+cout<<"son iguales";
+}
+return 0;
+}
