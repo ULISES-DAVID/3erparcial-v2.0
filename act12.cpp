75464dd3416efdcc3cd5c91926353226af892b37
PEREZ GARCIA ULISES DAVID <168507255+ULISES-DAVID@users.noreply.github.com>
2024-06-06 11:40:47 -0600

    Update act12.cpp

---
+++ act12.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int n1,n2,to;
+
+cout<<"dime tu primer numero numero";
+cin>>n1;
+cout<<"dime tú segundo numero numero";
+cin>>n2;
+to=n1%n2;
+cout<<"el total es: "<<to<<endl;
+if(to-0){;
+cout<<"no es divisible";
+}
+else{
+cout<<"numero divisible";
+
+
+}
+return 0;
+}
