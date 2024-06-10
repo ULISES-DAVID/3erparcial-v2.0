2eedd4db5bb530a18a95209e93bd670c71435ff3
PEREZ GARCIA ULISES DAVID <168507255+ULISES-DAVID@users.noreply.github.com>
2024-06-10 07:48:08 -0600

    Update act24.cpp

--- act24.cpp
+++ act24.cpp
@@ -5,13 +5,13 @@ int main ()
     int n,exponente,resultado;
     cout<<"Digite el numero ";
     cin>>n;
-    cout<<"Diigite el numero espomnente: ";
+    cout<<"pon el numero espomnente: ";
     cin>>exponente;
     resultado=n;
     for (int i; i!= exponente; i++)
     {
         resultado=resultado*n;
     }
-    cout<<"El resultado es "<<resultado;
+    cout<<"la respuesta es "<<resultado;
     return 0;
 }
